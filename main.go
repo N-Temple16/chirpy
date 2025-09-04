3e356bb4883f4438d700faf1a0c35b4fe7b1b42c
N-Temple16 <nigelatemple@gmail.com>
2025-09-04 14:31:22 -0400

    added stateful handlers

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func handleHealthz (w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -13,13 +26,33 @@ func handleHealthz (w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) handleNumRequests (w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	count := cfg.fileserverHits.Load()
+	body := fmt.Sprintf("Hits: %d\n", count)
+	w.WriteHeader(http.StatusOK)	
+	w.Write([]byte(body))
+}
+
+func (cfg *apiConfig) handleReset (w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)	
+	w.Write([]byte("OK"))
+}
+
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	apiCfg := &apiConfig{}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/healthz", handleHealthz)
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/metrics", apiCfg.handleNumRequests)
+	mux.HandleFunc("/reset", apiCfg.handleReset)
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 
 	server := &http.Server{
 		Addr:    ":" + port,
